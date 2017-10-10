4d13a758d0584ec09654762af502ab345fda941b
chibike <chibuikepraise@gmail.com>
2017-10-10 18:43:56 +0100

    Testing i2c 01

--- scripts/i2c_wrapper.py
+++ scripts/i2c_wrapper.py
@@ -5,14 +5,29 @@ import time
 
 class I2cObject(object):
     def __init__(self, address):
-        self.default_address = address
+        self.write_address = address
+        self.read_address = address | 0x01
         self.bus = smbus.SMBus(0)
 
     def write(self, register, data):
-        pass
+        if isinstance(data, type(list)) or isinstance(data, type(tuple)):
+        	pass
+        else:
+        	pass
 
     def read(self, register, howmany):
-        pass
+        self.bus.write_byte(self.read_address, register)
+
+        data = []
+        for i in xrange(howmany):
+        	data.append(self.bus.read_byte(self.read_address))
+
+        return data
+
+
+if __name__ == '__main__':
+	i2c_device = I2cObject(0x58)
+	print i2c_device.read(2, 4)
 
 
 # bus = smbus.SMBus(0)
